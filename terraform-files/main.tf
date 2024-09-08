f8e18b0439080e3fcf43d63124709f49942b6be4
MeHuman333 <104174853+MeHuman333@users.noreply.github.com>
2024-09-08 20:59:37 +0530

    Update main.tf

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -1,24 +1,30 @@
 resource "aws_instance" "test-server" {
-  ami = "ami-0e86e20dae9224db8"
+  ami           = "ami-0e86e20dae9224db8"
   instance_type = "t2.micro"
-  key_name = "keypair2"
-  vpc_security_group_ids = ["sg-07fd8b117d47d4a3c"]
+  key_name      = "keypair2"
+  vpc_security_group_ids = ["sg-07fd8b117d47d4a3c"]  # Updated security group ID
+  subnet_id     = "subnet-07824c9539dd6baed"  # Ensure this subnet ID is in vpc-0d4f69f9efe538ba8
+
   connection {
-     type = "ssh"
-     user = "ubuntu"
-     private_key = file("./keypair2.pem")
-     host = self.public_ip
-     }
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("./keypair2.pem")
+    host        = self.public_ip
+  }
+
   provisioner "remote-exec" {
-     inline = ["echo 'wait to start the instance' "]
+    inline = ["echo 'wait to start the instance' "]
   }
+
   tags = {
-     Name = "test-server"
-     }
-  provisioner "local-exec" {
-     command = "echo ${aws_instance.test-server.public_ip} > inventory"
-     }
-  provisioner "local-exec" {
-     command = "ansible-playbook /var/lib/jenkins/workspace/Banking-finance/terraform-files/ansibleplaybook.yml"
-     }
+    Name = "test-server"
   }
+
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.test-server.public_ip} > inventory"
+  }
+
+  provisioner "local-exec" {
+    command = "ansible-playbook /var/lib/jenkins/workspace/Banking-finance/terraform-files/ansibleplaybook.yml"
+  }
+}
