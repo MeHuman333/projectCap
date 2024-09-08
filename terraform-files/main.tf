6a18b532dcba38ab3feac64d03cac571d33dd8a3
MeHuman333 <104174853+MeHuman333@users.noreply.github.com>
2024-09-08 23:43:47 +0530

    Update main.tf

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -2,9 +2,8 @@ resource "aws_instance" "test-server" {
   ami           = "ami-0a0e5d9c7acc336f1"
   instance_type = "t2.micro"
   key_name      = "keypair2"
-  vpc_security_group_ids = ["sg-0b2cdab010111968c"]  # Updated security group ID
-  subnet_id     = subnet-045f7591518975b08
-
+  vpc_security_group_ids = ["sg-07fd8b117d47d4a3c"]  # Updated security group ID
+  
 
   connection {
     type        = "ssh"
